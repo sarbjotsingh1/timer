845653d51a9c190f71317290a0347c8ce0a5f470
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-26 22:16:21 +0200

    added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React from 'react';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
+import './styles.css';
+
 const App = () => {
   return (
     <section className='page'>
-      <div className='countdown'>
-        <FlipClockCountdown
-          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
-        />
-      </div>
+      <FlipClockCountdown
+        className='flip-clock'
+        to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+      />
     </section>
   );
 };
